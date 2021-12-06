30421bcdf2974a84461e78f085c16da2968c2901
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2021-12-06 13:26:35 +1000

    Test commit

--- ImperialPluginsConsole/Servicing/Interfaces/IHostedService.cs
+++ ImperialPluginsConsole/Servicing/Interfaces/IHostedService.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ImperialPluginsConsole.Servicing.Interfaces
+﻿namespace ImperialPluginsConsole.Servicing.Interfaces
 {
     public interface IHostedService
     {
         void Start();
+
         void Stop();
     }
 }
